fb632a2d24eec7d0f5e69b8d75a8cb89c4f43197
elias <eliasfritsch@hotmail.de>
2022-04-23 17:09:20 +0200

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Elias Fritsch on 23.04.22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
